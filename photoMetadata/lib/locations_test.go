7bc54a9de29037e6327ea186fd7723500411c4b4
sander-skjulsvik <sander@skjulsvik.com>
2024-10-13 17:54:22 +0200

    cleaned up TestGetCoordiantesByTime and edded over low threshold but under high

--- photoMetadata/lib/locations_test.go
+++ photoMetadata/lib/locations_test.go
@@ -2,6 +2,7 @@ package lib
 
 import (
 	"errors"
+	"runtime/debug"
 	"testing"
 	"time"
 
@@ -9,10 +10,16 @@ import (
 )
 
 func TestGetCoordiantesByTime(t *testing.T) {
-	t0 := time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC)
-	majorTimeDelta := time.Second
-	t1 := t0.Add(HIGH_TIME_DIFF_THRESHOLD + majorTimeDelta)
-	minorTimeDelta := time.Millisecond
+	var (
+		minorTimeDelta = time.Millisecond
+		majorTimeDelta = time.Second
+		t0             = time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC)
+		c0             = locationData.NewCoordinatesE2(0, 0)
+		t1             = t0.Add(HIGH_TIME_DIFF_THRESHOLD + majorTimeDelta)
+		c1             = locationData.NewCoordinatesE2(1, 1)
+		t2             = t0.Add(HIGH_TIME_DIFF_THRESHOLD * 4)
+		c2             = locationData.NewCoordinatesE2(2, 2)
+	)
 
 	ls := LocationStore{
 		LowTimeDiffThreshold:    LOW_TIME_DIFF_THRESHOLD,
@@ -21,12 +28,16 @@ func TestGetCoordiantesByTime(t *testing.T) {
 		SourceLocations: locationData.SourceLocations{
 			Locations: []locationData.LocationRecord{
 				{
-					Coordinates: locationData.NewCoordinatesE2(0, 0),
+					Coordinates: c0,
 					Time:        t0,
 				},
 				{
-					Coordinates: locationData.NewCoordinatesE2(1, 1),
+					Coordinates: c1,
 					Time:        t1,
+				},
+				{
+					Coordinates: c2,
+					Time:        t2,
 				},
 			},
 		},
@@ -35,130 +46,46 @@ func TestGetCoordiantesByTime(t *testing.T) {
 	checkResult := func(expectedCoordinates locationData.Coordinates, qTime time.Time, expectedDiff time.Duration, allowedCoordinateDelta float64, expectedErr error) {
 		coord, calcTimeDiff, err := ls.GetCoordinatesByTime(qTime)
 		if !errors.Is(err, expectedErr) {
-			t.Errorf("err not nil: %v", err)
+			t.Errorf("Expected error '%v' got '%v', for qTime: %s\n\n%s", expectedErr, err, qTime, debug.Stack())
+			return
 		}
 		if calcTimeDiff != expectedDiff {
-			t.Errorf("time diff not as expected:\n\texpected: %s\n\tcalculated: %s", expectedDiff, calcTimeDiff)
+			t.Errorf("time diff not as expected:\n\texpected: %s\n\tcalculated: %s\n\n%s", expectedDiff, calcTimeDiff, debug.Stack())
 		}
 
 		if !coord.EqualDelta(expectedCoordinates, allowedCoordinateDelta) {
 			t.Errorf(
-				"low time diff did not give coordinates equal to expected:\n\texpected: %s\n\tcalculated: %s",
-				expectedCoordinates.String(), coord.String(),
+				"low time diff did not give coordinates equal to expected:\n\texpected: %s\n\tcalculated: %s\n\n%s",
+				expectedCoordinates.String(), coord.String(), debug.Stack(),
 			)
 		}
 	}
 
 	// No diff
-	checkResult(locationData.NewCoordinatesE2(0, 0), t0, 0, 1e-7, nil)
-	checkResult(locationData.NewCoordinatesE2(1, 1), t1, 0, 1e-7, nil)
+	checkResult(c0, t0, 0, 1e-7, nil)
+	checkResult(c1, t1, 0, 1e-7, nil)
 
 	// Under low threshold
-	checkResult(locationData.NewCoordinatesE2(0, 0), t0.Add(minorTimeDelta), minorTimeDelta, 1e-7, nil)
-	checkResult(locationData.NewCoordinatesE2(0, 0), t0.Add(LOW_TIME_DIFF_THRESHOLD-minorTimeDelta), LOW_TIME_DIFF_THRESHOLD-minorTimeDelta, 1e-7, nil)
+	checkResult(c0, t0.Add(minorTimeDelta), minorTimeDelta, 1e-7, nil)
+	checkResult(c0, t0.Add(LOW_TIME_DIFF_THRESHOLD-minorTimeDelta), LOW_TIME_DIFF_THRESHOLD-minorTimeDelta, 1e-7, nil)
 
-	checkResult(locationData.NewCoordinatesE2(0, 0), t0.Add(-minorTimeDelta), minorTimeDelta, 1e-7, nil)
-	checkResult(locationData.NewCoordinatesE2(0, 0), t0.Add(-(LOW_TIME_DIFF_THRESHOLD - minorTimeDelta)), LOW_TIME_DIFF_THRESHOLD-minorTimeDelta, 1e-7, nil)
+	checkResult(c0, t0.Add(-minorTimeDelta), minorTimeDelta, 1e-7, nil)
+	checkResult(c0, t0.Add(-(LOW_TIME_DIFF_THRESHOLD - minorTimeDelta)), LOW_TIME_DIFF_THRESHOLD-minorTimeDelta, 1e-7, nil)
 
-	checkResult(locationData.NewCoordinatesE2(1, 1), t1.Add(minorTimeDelta), minorTimeDelta, 1e-7, nil)
-	checkResult(locationData.NewCoordinatesE2(1, 1), t1.Add(LOW_TIME_DIFF_THRESHOLD-minorTimeDelta), LOW_TIME_DIFF_THRESHOLD-minorTimeDelta, 1e-7, nil)
+	checkResult(c1, t1.Add(minorTimeDelta), minorTimeDelta, 1e-7, nil)
+	checkResult(c1, t1.Add(LOW_TIME_DIFF_THRESHOLD-minorTimeDelta), LOW_TIME_DIFF_THRESHOLD-minorTimeDelta, 1e-7, nil)
 
 	// Over low threshold but under high threshold
-	checkResult(locationData.NewCoordinatesE2(0, 0), t0.Add(LOW_TIME_DIFF_THRESHOLD+minorTimeDelta), LOW_TIME_DIFF_THRESHOLD+minorTimeDelta, 0.05, ErrTimeDiffMedium)
-	// ...
-	// over high treshold
+	lowerThreshold := LOW_TIME_DIFF_THRESHOLD + minorTimeDelta
+	checkResult(c0, t0.Add(lowerThreshold), lowerThreshold, 0.05, ErrTimeDiffMedium)
+	upperThreshold := MEDIUM_TIME_DIFF_THRESHOLD - minorTimeDelta
+	checkResult(c0, t0.Add(upperThreshold), upperThreshold, 0.2, ErrTimeDiffMedium)
 
-	// lrsLow := []locationData.LocationRecord{}
-	// lrsMed := []locationData.LocationRecord{}
-	// lrsHigh := []locationData.LocationRecord{}
-	// n := 3
-	// for i:= 0; i < n; i++ {
-	// 	lrsLow = append(lrsLow, locationData.LocationRecord{
-	// 		Coordinates: locationData.NewCoordinatesE2(float64(i), float64(i)),
-	// 		Time:        t0.Add((time.Duration(i)*LOW_TIME_DIFF_THRESHOLD) - majorTimeDelta),
-	// 	})
-	// 	lrsMed = append(lrsMed, locationData.LocationRecord{
-	// 		Coordinates: locationData.NewCoordinatesE2(float64(i), float64(i)),
-	// 		Time:        t0.Add((time.Duration(i)*MEDIUM_TIME_DIFF_THRESHOLD) - majorTimeDelta),
-	// 	})
-	// 	lrsHigh = append(lrsHigh, locationData.LocationRecord{
-	// 		Coordinates: locationData.NewCoordinatesE2(float64(i), float64(i)),
-	// 		Time:        t0.Add((time.Duration(i)*HIGH_TIME_DIFF_THRESHOLD) - majorTimeDelta),
-	// 	})
-	// }
+	// Over low threshold and outside of data
+	checkResult(locationData.Coordinates{}, t0.Add(-(LOW_TIME_DIFF_THRESHOLD + majorTimeDelta)), 0, 1e-7, ErrQuerytimeIsOutsideOfRange)
+	checkResult(locationData.Coordinates{}, t2.Add(LOW_TIME_DIFF_THRESHOLD+majorTimeDelta), 0, 1e-7, ErrQuerytimeIsOutsideOfRange)
 
-	// ls := LocationStore{
-	// 	LowTimeDiffThreshold:    LOW_TIME_DIFF_THRESHOLD,
-	// 	MediumTimeDiffThreshold: MEDIUM_TIME_DIFF_THRESHOLD,
-	// 	HighTimeDiffThreshold:   HIGH_TIME_DIFF_THRESHOLD,
-	// 	SourceLocations: locationData.SourceLocations{},
-	// }
-
-	// for i:= 0; i < n; i++ {
-	// 	// Low thresh
-	// 	ls0.SourceLocations.Locations = lrsLow
-	// 	checkResult()
-
-	// 	// Med thresh
-	// 	ls0.SourceLocations.Locations = lrsMed
-
-	// 	// High tresh
-	// 	ls0.SourceLocations.Locations = lrsHigh
-
-	// }
-
-	// lr0 := locationData.LocationRecord{
-	// 	Coordinates: locationData.NewCoordinatesE2(0, 0),
-	// 	Time:        t0,
-	// }
-	// lrLowTimeThresh := locationData.LocationRecord{
-	// 	Coordinates: locationData.NewCoordinatesE2(1, 1),
-	// }
-	// lrLowTimeThresh.Time = t0.Add(LOW_TIME_DIFF_THRESHOLD - 1*time.Minute)
-
-	// lrMedThresh := locationData.LocationRecord{
-	// 	Coordinates: locationData.NewCoordinatesE2(1, 1),
-	// }
-	// lrMedThresh.Time = t0.Add(MEDIUM_TIME_DIFF_THRESHOLD - 1*time.Minute)
-
-	// lrHighThresh := locationData.LocationRecord{
-	// 	Coordinates: locationData.NewCoordinatesE2(1, 1),
-	// }
-	// lrHighThresh.Time = t0.Add(HIGH_TIME_DIFF_THRESHOLD - 1*time.Minute)
-
-	// lrSuperHighThresh := locationData.LocationRecord{
-	// 	Coordinates: locationData.NewCoordinatesE2(1, 1),
-	// }
-	// lrSuperHighThresh.Time = t0.Add(HIGH_TIME_DIFF_THRESHOLD + 1*time.Minute)
-
-	// ls := LocationStore{
-	// 	LowTimeDiffThreshold:    LOW_TIME_DIFF_THRESHOLD,
-	// 	MediumTimeDiffThreshold: MEDIUM_TIME_DIFF_THRESHOLD,
-	// 	HighTimeDiffThreshold:   HIGH_TIME_DIFF_THRESHOLD,
-	// 	SourceLocations: locationData.SourceLocations{
-	// 		Locations: []locationData.LocationRecord{
-	// 			lr0, lrLowTimeThresh, lrMedThresh, lrHighThresh, lrSuperHighThresh,
-	// 		},
-	// 	},
-	// }
-
-	// // Before 0 time
-	// checkResult(lr0.Coordinates, lr0.Time.Add(-1 * time.Minute),  1 * time.Minute)
-
-	// // Low low time
-	// checkResult(lr0.Coordinates, lr0.Time.Add(1 * time.Minute),  1 * time.Minute)
-	// // high low time
-	// checkResult(lrLowTimeThresh.Coordinates, lrLowTimeThresh.Time.Add(-time.Minute),  -1 * time.Minute)
-
-	// // Med low
-	// checkResult(lrLowTimeThresh.Coordinates, lrLowTimeThresh.Time.Add(1 * time.Minute),  1 * time.Minute)
-	// // Med high
-	// checkResult(lrHighThresh.Coordinates, lrHighThresh.Time.Add(-time.Minute),  -1 * time.Minute)
-
-	// // High low
-
-	// // High high
-
-	// // Super high ?
+	// Over high threshold
+	// checkResult(locationData.Coordinates{}, t1.Add(HIGH_TIME_DIFF_THRESHOLD+majorTimeDelta), HIGH_TIME_DIFF_THRESHOLD+majorTimeDelta, 1e-7, ErrTimeDiffTooHigh)
 
 }
